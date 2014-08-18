1b1401e0f66468d1f58e6ff6871644c5e02a5993
Justas Trimailovas <j.trimailovas@gmail.com>
2014-08-18 18:29:17 +0300

    Created base template

--- src/akllt/urls.py
+++ src/akllt/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'akllt.views.home', name='home'),
+    url(r'^$', 'akllt.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
