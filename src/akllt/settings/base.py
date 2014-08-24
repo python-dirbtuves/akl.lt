8de5ac88c6852eb90825936d056053374954050b
sirex <sirexas@gmail.com>
2014-08-24 22:44:16 +0300

    Wagtail setup on Django 1.7.
    Wagtail officially not yet support Django 1.7, so here I used kaedroho:dj17_2
    [1] branch, with experimental Django 1.7 support.
    
    To setup whole project I followed official documentation [2] and consulted demo
    project examples [3].
    
    [1] https://github.com/kaedroho/wagtail/tree/dj17_2
    [2] http://wagtail.readthedocs.org/en/latest/howto/settings.html
    [3] https://github.com/torchbox/wagtaildemo

--- src/akllt/settings/base.py
+++ src/akllt/settings/base.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
+from django.conf import global_settings as defaults
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -31,12 +33,28 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'compressor',
+    'taggit',
+    'modelcluster',
+    'django.contrib.admin',
+
+    'wagtail.wagtailcore',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailforms',
+
     'akllt',
 )
 
@@ -48,6 +66,12 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+    'wagtail.wagtailredirects.middleware.RedirectMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = defaults.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
 )
 
 ROOT_URLCONF = 'akllt.urls'
@@ -82,4 +106,43 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'var', 'www', 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'var', 'www', 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+
+# django-compressor settings
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+# Auth settings
+LOGIN_URL = 'wagtailadmin_login'
+LOGIN_REDIRECT_URL = 'wagtailadmin_home'
+
+
+# Wagtail settings
+WAGTAIL_SITE_NAME = 'Atviras Kodas Lietuvai'
--- src/akllt/urls.py
+++ src/akllt/urls.py
@@ -1,10 +1,26 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf import settings
+
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'akllt.views.index', name='index'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^search/', include(wagtailsearch_frontend_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # For anything not caught by a more specific rule above, hand over to
+    # Wagtail's serving mechanism
+    url(r'', include(wagtail_urls)),
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.conf.urls.static import static
+
+    urlpatterns += (
+        staticfiles_urlpatterns() +
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
