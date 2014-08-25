4a37653dd6e419ad22f72cf2498026f392a853c7
Eimantas <eimantas.stonys@gmail.com>
2014-08-25 18:44:16 +0300

    Bootstrap integration (work in progress)

--- src/akllt/urls.py
+++ src/akllt/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +9,12 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.conf.urls.static import static
+
+    urlpatterns += (
+        staticfiles_urlpatterns() +
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
