449f027fe6ee4924a063dfc2750376101a59a9a9
Ignas Mikalajunas <ignas@uber.com>
2014-09-08 18:43:30 +0300

    Whitespace

--- src/akllt/views.py
+++ src/akllt/views.py
@@ -4,22 +4,26 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'base.html')
 
+
 def apie(request):
     return render(request, 'apie.html')
+
 
 def naujienos(request):
     return render(request, 'naujienos.html')
 
+
 def nuorodos(request):
     return render(request, 'nuorodos.html')
+
 
 def programos(request):
     return render(request, 'programos.html')
 
+
 def wiki(request):
     return render(request, 'wiki.html')
 
+
 def atviras_kodas(request):
     return render(request, 'atviras_kodas.html')
-
-
