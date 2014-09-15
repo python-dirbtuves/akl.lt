f1202657e18bcdfd626e7b7ecb57b603750e37d5
Justas Trimailovas <j.trimailovas@gmail.com>
2014-09-15 18:36:04 +0300

    Create functional test for data import

--- src/akllt/tests.py
+++ src/akllt/tests.py
@@ -1,5 +1,12 @@
+# coding: utf-8
+import pkg_resources
+
 from django.test.testcases import TransactionTestCase
 from homophony import BrowserTestCase, Browser
+
+
+def import_pages(directory):
+    pass
 
 
 class SmokeTest(TransactionTestCase):
@@ -15,3 +22,16 @@ class FoobarTestCase(BrowserTestCase):
         browser.open('http://testserver')
         browser.getLink('Naujienos').click()
         self.assertEquals(browser.title, 'Atviras Kodas Lietuvai')
+
+
+class ImportTestCase(BrowserTestCase):
+
+    def test_import(self):
+        import_pages(pkg_resources
+                     .resource_filename('akllt', 'test_data/pages'))
+        browser = Browser()
+        browser.open('http://testserver')
+        browser.getLink('Apie').click()
+        # expected_content = pkg_resources.resource_string(
+        #     'akllt', 'test_data/pages/apie.html')
+        # self.assertTrue(expected_content in browser.contents)
