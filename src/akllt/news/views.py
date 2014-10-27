e296cefacae7154fd4060ec76abb9ddefb6cd763
Euphorbium <compositum@gmail.com>
2014-10-27 20:06:12 +0200

    Add view for news details

--- src/akllt/news/views.py
+++ src/akllt/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from akllt.models import NewsStory
 
@@ -6,4 +6,11 @@ from akllt.models import NewsStory
 def news_items(request):
     return render(request, 'akllt/news/news_items.html', {
         'news_items': NewsStory.objects.all()[:20]
+    })
+
+
+def news_details(request, news_item_id):
+    news_item = get_object_or_404(NewsStory, pk=news_item_id)
+    return render(request, 'akllt/news/news_details.html', {
+        'news_item': news_item,
     })
