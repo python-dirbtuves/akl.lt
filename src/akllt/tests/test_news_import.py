7a499ce6e39ea0b106181095fef268c6f73f1f8b
Mantas <sirexas@gmail.com>
2014-10-27 18:52:11 +0200

    Remove redundant test

--- src/akllt/tests/test_news_import.py
+++ src/akllt/tests/test_news_import.py
@@ -101,24 +101,6 @@ class NewsExportReadTests(unittest.TestCase):
         self.assertIsNone(news_items[0]['date'])
 
 
-class NewsImportTests(django.test.TestCase):
-    def test_create_news(self):
-        self.assertEqual(NewsStory.objects.count(), 0)
-        root = Page.add_root(title='Root page')
-
-        news_folder = pkg_resources.resource_filename(
-            'akllt', 'tests/fixtures/naujienos'
-        )
-        news = import_news(news_folder)
-        for news_story in news:
-            root.add_child(instance=NewsStory(
-                title=news_story['title'],
-                date=news_story['date'],
-                blurb=news_story['blurb'],
-                body=news_story['body'],
-            ))
-
-
 class NewsImportCommandTests(django.test.TestCase):
     def test_command(self):
         self.assertEqual(NewsStory.objects.count(), 0)
