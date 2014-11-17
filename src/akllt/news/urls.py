d256c80cd155c08d854e5205868677ee9d34d8b5
Mantas <sirexas@gmail.com>
2014-11-17 18:31:54 +0200

    Fix pylint/pep8 errors
    Also did small refactoring to normalize view function naming.

--- src/akllt/news/urls.py
+++ src/akllt/news/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'akllt.news.views',
     url(r'^$', 'news_items', name='news_items'),
-    url(r'news/(?P<slug>[a-z\d-]+)', 'news_item', name='news_item'),
+    url(
+        r'news/(?P<slug>[a-z\d-]+)', 'news_item_details',
+        name='news_item_details'
+    ),
 )
--- src/akllt/news/views.py
+++ src/akllt/news/views.py
@@ -8,14 +8,9 @@ def news_items(request):
         'news_items': NewsStory.objects.all()[:20]
     })
 
-def news_item(request, slug):
-    news_item = get_object_or_404(NewsStory, slug=slug)
-    return render(request, 'akllt/news/news_item.html', {
-        'news_item': news_item,
-    })
 
-def news_details(request, news_item_id):
-    news_item = get_object_or_404(NewsStory, pk=news_item_id)
-    return render(request, 'akllt/news/news_details.html', {
+def news_item_details(request, slug):
+    news_item = get_object_or_404(NewsStory, slug=slug)
+    return render(request, 'akllt/news/news_item_details.html', {
         'news_item': news_item,
     })
