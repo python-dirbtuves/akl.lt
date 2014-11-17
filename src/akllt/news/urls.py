c104eeaba78d3ce0fdce61273296ed41107c1e64
Mantas <sirexas@gmail.com>
2014-11-17 18:25:08 +0200

    Fix failing tests
    What was failing:
    
    - news item view was missing slug argument
    
    - news item url was to global
    
    - standard_page.html template was moved to pages app

--- src/akllt/news/urls.py
+++ src/akllt/news/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'akllt.news.views',
     url(r'^$', 'news_items', name='news_items'),
-    url(r'(?P<slug>[a-z\d-]+)', 'news_item', name='news_item'),
+    url(r'news/(?P<slug>[a-z\d-]+)', 'news_item', name='news_item'),
 )
--- src/akllt/news/views.py
+++ src/akllt/news/views.py
@@ -8,9 +8,10 @@ def news_items(request):
         'news_items': NewsStory.objects.all()[:20]
     })
 
-def news_item(request):
-    return render(request, 'akllt/news/news_items.html', {
-        'news_items': NewsStory.objects.all()[:20]
+def news_item(request, slug):
+    news_item = get_object_or_404(NewsStory, slug=slug)
+    return render(request, 'akllt/news/news_item.html', {
+        'news_item': news_item,
     })
 
 def news_details(request, news_item_id):
