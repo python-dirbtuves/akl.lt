f5580f5aa68b7c8fa628220108da5e898b655aeb
Mantas <sirexas@gmail.com>
2015-01-21 16:42:58 +0200

    Remove few stubs

--- akllt/stub/urls.py
+++ akllt/stub/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'akllt.stub.views',
-    url(r'^nuorodos', 'nuorodos', name='nuorodos'),
-    url(r'^atviras_kodas', 'atviras_kodas', name='atviras_kodas'),
     url(r'^wiki', 'wiki', name='wiki'),
     url(r'^programos', 'programos', name='programos'),
 )
