431400b0f7c19227fb77a422e9bb482484e0058c
Mantas <sirexas@gmail.com>
2015-02-03 14:50:27 +0200

    Fixes #15

--- akllt/dataimport/tests/test_redirects.py
+++ akllt/dataimport/tests/test_redirects.py
@@ -1,32 +1,14 @@
 from django_webtest import WebTest
-from wagtail.wagtailcore.models import Site
-from wagtail.wagtailredirects.models import Redirect
-
-from akllt.news.models import NewsIndex
-from akllt.news.models import NewsStory
 
 
-class RedirectTests(WebTest):
-    def test_redirect(self):
-        """Test news redirect"""
-        site = Site.objects.get(is_default_site=True)
-        news_index = site.root_page.add_child(instance=NewsIndex(
-            title='Naujienos',
-            slug='naujienos',
-        ))
-        new_page = news_index.add_child(instance=NewsStory(
-            title='Naujiena 200',
-            slug='naujiena_200',
-        ))
-        Redirect.objects.create(
-            old_path='/naujienos/?id=200',
-            site=site,
-            is_permanent=True,
-            redirect_page=new_page,
-        )
+class LegacyRedirectTests(WebTest):
+    def assertRedirects(self, resp, url):
+        self.assertEqual(resp.location, 'http://localhost:80%s' % url)
 
-        # Open naujienos/?id=200
-        page = self.app.get('/naujienos/', 'id=200', status=301)
-        self.assertEqual(
-            page.location, 'http://localhost:80/naujienos/naujiena_200'
-        )
+    def test_news_redirect(self):
+        resp = self.app.get('/naujienos/', 'id=naujiena_200', status=301)
+        self.assertRedirects(resp, '/naujienos/naujiena_200/')
+
+    def test_page_redirect(self):
+        resp = self.app.get('/ak/', 'doc=osd.html', status=301)
+        self.assertRedirects(resp, '/ak/osd/')
--- akllt/website/middleware.py
+++ akllt/website/middleware.py
@@ -2,10 +2,22 @@ from django import http
 
 
 class LegacyRedirectMiddleware(object):
-    def process_response(self, request, response):
+    def process_request(self, request):
+        """Redirect legacy links to new ones
+
+        Currently this middleware handles these types of legacy links:
+
+        - /ak/?doc=osd.html -> /ak/osd/
+
+        - /naujienos/?id=naujiena_1047 -> /naujienos/naujiena_1047/
+
+        """
         if request.path == '/naujienos/' and 'id' in request.GET:
-            return http.HttpResponsePermanentRedirect(
-                '/naujienos/naujiena_%s' % request.GET['id']
-            )
+            url = '/naujienos/%s/' % request.GET['id']
+        elif 'doc' in request.GET:
+            doc = request.GET['doc']
+            tail = doc[:-5] if doc.endswith('.html') else doc
+            url = '%s%s/' % (request.path, tail)
         else:
-            return response
+            url = None
+        return url and http.HttpResponsePermanentRedirect(url)
