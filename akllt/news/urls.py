5577c5b1febb6cfd53718aa31b35de953402e91e
Mantas <sirexas@gmail.com>
2015-05-25 18:08:06 +0300

    Submit news story (work in progress)

--- akllt/news/urls.py
+++ akllt/news/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
 from akllt.news.feeds import LatestNewsFeed
+from akllt.news import views
 
 
 urlpatterns = patterns(
     '',
     url(r'^feed/$', LatestNewsFeed(), name='rss'),
+    url(r'^news/create/$', views.NewsStoryCreate.as_view(), name='news_create'),
 )
--- akllt/news/views.py
+++ akllt/news/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.edit import CreateView
+from django.utils.text import slugify
 
 from akllt.news.models import NewsStory
+from akllt.news.services import get_news_index_page
 
 
 def news_items(request):
@@ -14,3 +17,23 @@ def news_item_details(request, slug):
     return render(request, 'akllt/news/news_item_details.html', {
         'news_item': news_item,
     })
+
+
+class NewsStoryCreate(CreateView):
+    template_name = 'news/news_form.html'
+    model = NewsStory
+    fields = ('title', 'body')
+
+    def form_valid(self, form):
+        page = form.save(commit=False)
+        page.slug = slugify(page.title)
+
+        news_index_page = get_news_index_page()
+        news_index_page.add_child(instance=page)
+
+        page.save_revision(
+            user=self.request.user,
+            submitted_for_moderation=True,
+        )
+
+        return redirect('/')
